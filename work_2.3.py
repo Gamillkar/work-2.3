b0fb4a9fc09fa624faad4c52e01b20d7c2f0b813
Egor Siropov <stafffsss@mail.ru>
2020-05-17 01:44:11 +0300

    ver 2.0

--- work_2.3.py
+++ work_2.3.py
@@ -1,32 +1,29 @@
 number = input("Введите выражение ").split(' ')
-try: # задание №2 деление на строки
+try: # задание №2 операции не с числами
     x = int(number[1])
     s = int(number[2])
+    arithmetic_operation = ['+', '-', '/', '*']
     assert len(number) < 4, 'Введено больше 3 символов' # задание № 2
+    assert  number[0] in arithmetic_operation, 'первый элемент не арифметическая орперация'
 except ValueError as ex:
     print('операции со строками невозможны', ex)
 
+signs = number.pop(0)
+num = []
 
-else:
-    signs = number.pop(0)
-    num = []
-
-    for sign in number:
-        num.append(int(sign))
-
-    if signs == '-':
-        print(num[0] - (num[1]))
-    elif signs == '+':
-        print(num[0] + (num[1]))
-    elif signs == '/':
-        try: # задание №2 деление на ноль
-            assert int(num[1]) > 0, 'На ноль делить нельзя'
-        except Exception as e:
-            print(e)
-        else:
-            print(num[0] / (num[1]))
-
-    elif signs == '*':
-        print(num[0] * (num[1]))
+for sign in number:
+    num.append(int(sign))
+if signs == '-':
+    print(num[0] - (num[1]))
+elif signs == '+':
+    print(num[0] + (num[1]))
+elif signs == '/':
+    try: # задание №2 деление на ноль
+        assert 0 > int(num[1]) or 0 < int(num[1]), 'На ноль делить нельзя'
+    except Exception as e:
+        print(e)
+    print(num[0] / (num[1]))
+elif signs == '*':
+    print(num[0] * (num[1]))
 
 
--- work_2.py
+++ work_2.py
@@ -1,7 +1,4 @@
 
-
-
-
 documents = [
     {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
     {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
@@ -13,13 +10,7 @@ documents = [
 def name():
     try:
         for docs in documents:
-            for key, values in docs.items():
-                if key == 'name':
-                    print(values)
-            docs['name']
+            print(docs['name'])
     except KeyError as e:
         print('В одном из полей отсутствует ключ', e)
-
-        return values
-
 name()
